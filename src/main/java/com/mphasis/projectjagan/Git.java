7feed0e81b562b63fb95e69a6d7c4ad220a219a8
Jagan <jagant48@gmail.com>
2025-02-03 10:32:13 +0530

    updated 1

--- src/main/java/com/mphasis/projectjagan/Git.java
+++ src/main/java/com/mphasis/projectjagan/Git.java
@@ -1,9 +1,15 @@
 package com.mphasis.projectjagan;
 
 public class Git {
-
+     static int add(int a,int b) {
+    	 int c=a+b;
+    	 System.out.println("the addition"+" "+c);
+    	 return a+b;
+    	 
+     }
 	public static void main(String[] args) {
          System.out.println("Git Commands");
+         add(10,5);
 	}
 
 }
