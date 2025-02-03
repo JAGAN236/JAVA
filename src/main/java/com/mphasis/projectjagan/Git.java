fdc8dd3ddc34ea478168b7ae637477e37e919149
Jagan <jagant48@gmail.com>
2025-02-03 10:47:31 +0530

    main code

--- src/main/java/com/mphasis/projectjagan/Git.java
+++ src/main/java/com/mphasis/projectjagan/Git.java
@@ -1,15 +1,10 @@
 package com.mphasis.projectjagan;
 
 public class Git {
-     static int add(int a,int b) {
-    	 int c=a+b;
-    	 System.out.println("the addition"+" "+c);
-    	 return a+b;
-    	 
-     }
+
 	public static void main(String[] args) {
-         System.out.println("phase1 updated one");
-         add(10,5);
+         System.out.println("M");
+ 
 	}
 
 }
